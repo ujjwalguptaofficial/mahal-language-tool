ed49721d8f13f95ee0922927b42a720caf536a3e
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2022-03-26 08:16:10 +0530

    fix issue with getSnapShot not working in window

--- language_service/server/utils/get_file_path_from_url.ts
+++ language_service/server/utils/get_file_path_from_url.ts
@@ -1,19 +1,23 @@
-import { pathToFileURL, URL } from "url"
+import { platform } from "os";
+import { Utils, URI } from 'vscode-uri';
 
-export const getFilePathFromURL = (value: string) => {
-    try {
-        const url = new URL(value);
-        return url.pathname;
-    } catch (error) {
-
+const IS_WINDOWS = platform() === 'win32';
+export const getFilePathFromURL = (documentUri: string) => {
+    if (IS_WINDOWS) {
+        // Windows have a leading slash like /C:/Users/pine
+        // vscode-uri use lower-case drive letter
+        // https://github.com/microsoft/vscode-uri/blob/95e03c06f87d38f25eda1ae3c343fe5b7eec3f52/src/index.ts#L1017
+        return URI.parse(documentUri).path.replace(/^\/[a-zA-Z]/, (s: string) => s.slice(1));
+    } else {
+        return URI.parse(documentUri).path;
     }
-    return value;
 }
-export const getURLFromPath = (value: string) => {
+export const getURLFromPath = (fsPath: string) => {
     try {
-        return pathToFileURL(value).toString();
+        return URI.file(fsPath).toString();
+        // pathToFileURL(value).toString();
     } catch (error) {
 
     }
-    return value;
+    return fsPath;
 }
